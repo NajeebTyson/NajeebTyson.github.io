e8c65b4a65e7bb19b4b8687c66ade67722fbb68c
NajeebTyson <najeebus777@gmail.com>
2021-02-12 01:50:50 +0500

    done about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'HTML & (S)CSS', 'React', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['C++', 'Rust', 'Python', 'Javascript', 'Node.js', 'PostgreSql & MongoDB'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -141,19 +141,19 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>Hello! I'm Brittany, a software engineer based in Boston, MA.</p>
+            <p>Hello! I'm Najeeb, a software engineer based in Islamabad, Pakistan.</p>
 
             <p>
-              I enjoy creating things that live on the internet, whether that be websites,
-              applications, or anything in between. My goal is to always build products that provide
-              pixel-perfect, performant experiences.
+              I enjoy creating softwares specially when they are in the initial stages, after that sometimes I cursed
+              myself for getting me in to this profession :P. Anyways jokes aside, I love my profession and I love the
+              technologies that most of the people run from like C++. Mainly I work on back-end of systems.
             </p>
 
             <p>
-              Shortly after graduating from{' '}
-              <a href="https://www.ccis.northeastern.edu">Northeastern University</a>, I joined the
-              engineering team at <a href="https://www.upstatement.com">Upstatement</a> where I work
-              on a wide variety of interesting and meaningful projects on a daily basis.
+              Right after graduating from{' '}
+              <a href="https://www.nu.edu.pk">FAST University</a>, I joined a startup named <a href="https://www.automotive-ai.com">
+              Automotive Artificial Intelligence</a> where I work
+              on a wide variety of interesting and meaningful projects.
             </p>
 
             <p>Here are a few technologies I've been working with recently:</p>
